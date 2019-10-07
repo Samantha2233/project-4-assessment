0712788e74dea0df564326e3b864490eba0b3b0b
Samantha <sami.shackelford@gmail.com>
2019-10-07 14:19:03 -0700

    Updates

---
+++ precache-manifest.6b32ffb7f69fb0ddce0427af6c21e7ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef312686acd6bcde2644b52a4cba96ce",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "950a536d6c1cdd366d71",
+    "url": "/project-4-assessment/static/css/main.5a4bed1f.chunk.css"
+  },
+  {
+    "revision": "70b565d8c2a153c637e4",
+    "url": "/project-4-assessment/static/js/2.49750bb4.chunk.js"
+  },
+  {
+    "revision": "950a536d6c1cdd366d71",
+    "url": "/project-4-assessment/static/js/main.9bed78db.chunk.js"
+  },
+  {
+    "revision": "6f572fbbe798c8e20162",
+    "url": "/project-4-assessment/static/js/runtime-main.ae4e6a17.js"
+  }
+]);
